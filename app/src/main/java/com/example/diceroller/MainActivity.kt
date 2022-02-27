a5ad2bedc06be731304d50c15ab8dd6d2166cd00
tintayadev <pau.tintaya.code@gmail.com>
2022-02-27 19:19:00 -0400

    Second dice added

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -32,10 +32,23 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
+        val dice2 = Dice(6)
+        val diceRoll2 = dice.roll()
+
         // Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
 
-        // Determine which drawable resource ID to use based on the dice roll
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(randomDrawableResource(diceRoll))
+        diceImage2.setImageResource(randomDrawableResource(diceRoll2))
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
+        diceImage2.contentDescription = diceRoll2.toString()
+    }
+    // Determine which drawable resource ID to use based on the dice roll
+    private fun randomDrawableResource(diceRoll : Int): Int {
         val drawableResource = when (diceRoll) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -44,12 +57,7 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
-        // Update the ImageView with the correct drawable resource ID
-        diceImage.setImageResource(drawableResource)
-
-        // Update the content description
-        diceImage.contentDescription = diceRoll.toString()
+        return drawableResource
     }
 }
 class Dice(private val numSides: Int) {
